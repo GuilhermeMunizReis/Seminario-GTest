13d9ec3139b95d18699c388fb31486331e7319d4
GuilhermeMunizReis <guilhermemuniz1001@gmail.com>
2024-11-12 09:24:58 -0300

    Restaurando o trem de novo

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Guilherme/source/repos/Seminario-GTest
-# Build directory: C:/Users/Guilherme/source/repos/Seminario-GTest/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(MeuCodigoTests "C:/Users/Guilherme/source/repos/Seminario-GTest/build/tests.exe")
-set_tests_properties(MeuCodigoTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Guilherme/source/repos/Seminario-GTest/CMakeLists.txt;22;add_test;C:/Users/Guilherme/source/repos/Seminario-GTest/CMakeLists.txt;0;")
