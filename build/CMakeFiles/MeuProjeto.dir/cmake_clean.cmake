0a77e45e6129254814de99738466fc56c11dff45
GuilhermeMunizReis <guilhermemuniz1001@gmail.com>
2024-11-12 01:19:13 -0300

    Atualizando o CMakeLists 3

---
+++ build/CMakeFiles/MeuProjeto.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MeuProjeto.dir/src/character.cpp.obj"
+  "CMakeFiles/MeuProjeto.dir/src/character.cpp.obj.d"
+  "CMakeFiles/MeuProjeto.dir/src/item.cpp.obj"
+  "CMakeFiles/MeuProjeto.dir/src/item.cpp.obj.d"
+  "MeuProjeto.exe"
+  "MeuProjeto.exe.manifest"
+  "MeuProjeto.pdb"
+  "libMeuProjeto.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MeuProjeto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
